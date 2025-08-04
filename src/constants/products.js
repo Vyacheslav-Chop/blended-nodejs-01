c217c5769ba84eaaa2045061b83dfd79c150cd06
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-08-04 18:23:22 +0300

    feat: generate JSON files for each product

--- src/constants/products.js
+++ src/constants/products.js
@@ -2,4 +2,4 @@ import path from 'node:path';
 
 export const PATH_DB = path.join(process.cwd(), 'src', 'db', 'db.json');
 
-export const PATH_FILES_DIR = path.join(process.cwd(), 'src', 'files');
+export const PATH_FILES_DIR = path.join(process.cwd(), 'src', 'db', 'files');
--- src/scripts/createProductsFiles.js
+++ src/scripts/createProductsFiles.js
@@ -1,8 +1,20 @@
-import { readProducts } from "../utils/readProducts.js";
+import { writeFile } from 'node:fs/promises';
+import { PATH_FILES_DIR } from '../constants/products.js';
+import { readProducts } from '../utils/readProducts.js';
+import path from 'node:path';
 
 const createProductsFiles = async () => {
   const data = await readProducts();
 
+  for (const product of data) {
+    const fileName = `${product.name.toLowerCase().replace(/\s+/g, '-')}.json`;
+
+    const filePath = path.join(PATH_FILES_DIR, fileName);
+
+    console.log({ fileName, filePath });
+
+    await writeFile(filePath, JSON.stringify(product, null, 2));
+  }
 };
 
 createProductsFiles();
